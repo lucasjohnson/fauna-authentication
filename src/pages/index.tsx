2b93fa3b70d9b43af272ecf45628d9ca2424b322
Lucas Johnson <contact@lucasjohnson.co.nz>
2022-01-10 20:20:09 +0100

    auth and resource logic in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import { motion } from 'framer-motion';
-import { SiteQuery } from '../interfaces';
+import faunadb from 'faunadb';
+import { SiteQuery, Resources, Resource } from '../interfaces/';
+import { FaunaIndex, AnchorVariant } from '../enums/';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/Head/SEO';
-import { Paragraph } from '../emotion/typography';
+import Form from '../components/Form';
+import Anchor from '../components/Anchor';
+import { Paragraph, Heading } from '../emotion/typography';
 import GatsbyIcon from '../assets/svg/gatsby.svg';
 
 const IndexPage: React.FC = () => {
@@ -14,26 +17,73 @@ const IndexPage: React.FC = () => {
         siteMetadata {
           title
           description
+          errorMessage
         }
       }
     }
   `);
+  const [token, setToken] = useState<string>('');
+  const [resources, setResources] = useState();
+
+  useEffect(() => {
+    if (token) {
+      const q = faunadb.query;
+      const userClient = new faunadb.Client({
+        secret: token,
+      });
+
+      userClient
+        .query(
+          q.Map(
+            q.Paginate(q.Match(q.Index(FaunaIndex.ALL_RESOURCES))),
+            q.Lambda((x: any) => q.Get(x))
+          )
+        )
+        .then((reponse: Resources) => {
+          setResources(reponse.data);
+        })
+        .catch((error: {}) => console.error(error));
+    }
+  }, [token]);
+
+  const renderForm = () => {
+    return (
+      <React.Fragment>
+        <GatsbyIcon />
+        <Form
+          setToken={setToken}
+          errorMessage={site.siteMetadata.errorMessage}
+        />
+      </React.Fragment>
+    );
+  };
+
+  const renderResources = () => {
+    return (
+      <React.Fragment>
+        <Heading>Resources</Heading>
+        <ul>
+          {resources.map((resource: Resource, key: number) => (
+            <li key={key}>
+              <Anchor
+                title={resource.data.title}
+                url={resource.data.url}
+                variant={AnchorVariant.EXTERNAL}
+              >
+                {resource.data.title}
+              </Anchor>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  };
 
   return (
     <Layout>
       <SEO pageTitle={site.siteMetadata.title} />
-      <motion.div
-        className="logo"
-        animate={{ rotate: 360 }}
-        transition={{
-          duration: 4,
-          repeat: Infinity,
-          repeatDelay: 0.25,
-        }}
-      >
-        <GatsbyIcon />
-      </motion.div>
       <Paragraph>{site.siteMetadata.description}</Paragraph>
+      {!token ? renderForm() : resources && renderResources()}
     </Layout>
   );
 };
